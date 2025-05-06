af99237b86a77651909bd516922ac6c1f5b7a978
LuisCrespoH <luiscrespoherrera@gmail.com>
2025-05-06 21:52:50 +0200

    Logs de errores 5XX en Slack

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors")
 require('dotenv').config();
+const morganBody = require("morgan-body");
+const loggerStream = require("./utils/handleLogger")
 const swaggerUi = require("swagger-ui-express")
 const swaggerSpecs = require("./docs/swagger")
 
@@ -15,6 +17,14 @@ app.use("/api-docs",
     swaggerUi.setup(swaggerSpecs)
 )
 app.use('/api', routers)
+
+morganBody(app, {
+    noColors: true,
+    skip: function(req, res) {
+    return res.statusCode < 400
+    },
+    stream: loggerStream
+})
 
 //app.use(express.static("storage"))
 const port = process.env.PORT || 3000
